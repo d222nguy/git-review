c1fe8db67844a8dcab6b8c3e3efdb9573e71d32d
Duy.Nguyen <duydn95@gmail.com>
2021-09-28 11:16:23 -0400

    init commit

---
+++ test_tree.py
@@ -0,0 +1,17 @@
+#Non-recursive definition
+class Node:
+    def __init__(self, key, children = []):
+        self.key = key
+        self.chidlren = children
+class Tree:
+    def __init__(self):
+        self.root = Node("United Kingdom")
+        self.root.children = [Node("Wales"), Node("North Ireland"), \
+            Node("Scotland"), Node("England")]
+    def height_of(u):
+        if u is None:
+            return 0
+        ans = 0
+        for v in u.children:
+            ans = max(ans, height_of(v) + 1)
+        return ans
